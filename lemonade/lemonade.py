d95bf953fa564f06c0281001714835e1bd3b9172
ItsFuchsia <havardfarden@gmail.com>
2025-03-25 22:15:11 +0100

    added lemonade challenge and it works me thinks

---
+++ lemonade/lemonade.py
@@ -0,0 +1,16 @@
+cashmoney = {
+    5: 0,
+    10: 0,
+    20: 0
+}
+
+def lemonade(list):
+    for i in list:
+        cashmoney[i] += 1
+        if (i == 10 and cashmoney[5] == 0) or i == 20 and (cashmoney[5] < 3 or (cashmoney[10] == 0 and cashmoney[5] == 0)):
+            return(False)
+
+    return(True)
+
+
+print(lemonade([5, 5, 5, 20]))
